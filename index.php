0566b803c0fd5b502b0171345142d394d1047ad7
koffi <koffi.balayn@gmail.com>
2022-06-01 23:51:51 +0200

    Ajout titre

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Gros titre</title>
     </head>
     <body>
         <?php
