7c3f4cc5afccab7f71bb17539bbe4f2e3c69d5d8
koffi <koffi.balayn@gmail.com>
2022-06-02 16:08:19 +0200

    Ajout accueil

--- app/view/viewAccueil.php
+++ app/view/viewAccueil.php
@@ -1,7 +1,21 @@
-<?php
+ 
+<!-- ----- debut de la page acceuil -->
+<?php include 'fragment/fragmentHeader.html'; ?>
+<body>
+  <div class="container">
+    <?php
+    include 'fragment/fragmentMenu.html';
+    include 'fragment/fragmentJumbotron.html';
+    ?>
+  </div>   
+  
+  
+  <?php
+  include 'fragment/fragmentFooter.html';
+  ?>
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+  <!-- ----- fin de la page acceuil -->
+
+</body>
+</html>
 
